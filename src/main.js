1363b904cc13a09d47b331d278e9c1050f9e83c1
cuiqijia <1097993489@qq.com>
2019-08-15 15:43:32 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 职责1 导入项目依赖的资源 (包 css ...)
+// 职责2. 初始化根实例 new Vue() 选项中的render 作用是把某一个A组件渲染在你指定的容器内(.$mout('#app))内
 new Vue({
   render: h => h(App)
 }).$mount('#app')
